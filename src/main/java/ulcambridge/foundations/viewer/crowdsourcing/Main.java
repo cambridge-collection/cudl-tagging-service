df7a184107b173c6ac079e3c9077b9659a21a741
Hal Blackburn <hwtb2@cam.ac.uk>
2016-11-10 14:27:03 +0000

    Set up Spring Boot pom

---
+++ src/main/java/ulcambridge/foundations/viewer/crowdsourcing/Main.java
@@ -0,0 +1,14 @@
+package ulcambridge.foundations.viewer.crowdsourcing;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+}
